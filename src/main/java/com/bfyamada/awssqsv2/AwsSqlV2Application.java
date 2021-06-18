1a9d4c2cb2d9436adb6b52caf9ceca14166eecab
Bruno Yamada <yamada_bruno@hotmail.com>
2021-06-17 22:54:51 -0300

    First commit

---
+++ src/main/java/com/bfyamada/awssqsv2/AwsSqlV2Application.java
@@ -0,0 +1,13 @@
+package com.bfyamada.awssqsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSqlV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSqlV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bfyamada/awssqsv2/AwsSqlV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfyamada.awssqsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqlV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
